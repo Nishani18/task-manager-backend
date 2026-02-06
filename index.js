b8381adad6d10600b3db5c791bb1982e5b119437
Nishani18 <nishanir18@gmail.com>
2026-02-06 12:47:38 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [process.env.CLIENT_URL],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type"],
     credentials: true,
   }),
 );
@@ -43,8 +43,6 @@ app.get("/", (req, res) => {
 // Error Handling Middleware
 app.use(notFound);
 app.use(errorHandler);
-
-connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
